38d47480f5f10559676718c74fe7d6f44ae623c4
Gaston <grodriguez@itba.edu.ar>
2018-02-28 11:00:18 -0300

    Book model
    You can now create a book. example:
    irb(main):001:0> Book.create(genre: "fantasy", author: "yo", image: "qqq",\
     title: "el nombre del viento", publisher: "someone", year: "last year")

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228125554) do
+ActiveRecord::Schema.define(version: 20180228135534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "genre", null: false
+    t.string "author", null: false
+    t.string "image", null: false
+    t.string "title", null: false
+    t.string "publisher", null: false
+    t.string "year", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,8 +40,19 @@ ActiveRecord::Schema.define(version: 20180228125554) do
     t.string "last_name"
     t.string "string"
     t.string "first_name"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.json "tokens"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
