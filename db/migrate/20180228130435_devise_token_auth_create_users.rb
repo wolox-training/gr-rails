b96c77198587aa92b2e2a6af5ef51346720820c7
Gaston <grodriguez@itba.edu.ar>
2018-02-28 13:36:46 -0300

    fixed everything. you can now login

--- db/migrate/20180228130435_devise_token_auth_create_users.rb
+++ db/migrate/20180228130435_devise_token_auth_create_users.rb
@@ -5,25 +5,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
       t.string :provider, :null => false, :default => "email"
       t.string :uid, :null => false, :default => ""
 
-      ## Database authenticatable
-
-      ## Recoverable
-
-      ## Rememberable
-
-      ## Trackable
-
-      ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      #t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
       ## User Info
       t.string :name
       t.string :nickname
@@ -35,7 +16,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
     end
 
     add_index :users, [:uid, :provider],     unique: true
-    add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,       unique: true
   end
 end
