00709275d73b1e33f359fdbfa1e91cb31479cc46
Gaston <grodriguez@itba.edu.ar>
2018-03-01 11:34:24 -0300

    Modified the rent_spec test

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -1,5 +1,32 @@
-require 'rails_helper'
+require 'faker'
+describe Rent do
+  subject(:rent) do
+    Rent.new(user_id: user_id, book_id: user_id, from: from, to: to)
+  end
+  let(:user_id) { Faker::Number.between(1,1) } #Could be between 1 and Users.all.size
+  let(:book_id) { Faker::Number.between(1,1) }
+  let(:from)    { Faker::Date.backward(10) }
+  let(:to)      { Faker::Date.forward(10)  }
+  
+  it do
+    is_expected.to be_valid
+  end
+  
+  describe '#create' do
+    context 'when user_id is nil' do
+      let(:user_id) { nil }
+     
+      it do
+        is_expected.to be_invalid
+      end
+  end
+ 
+  context 'when book_id is nil' do
+    let(:book_id) { nil }
 
-RSpec.describe Rent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it do
+      is_expected.to be_invalid
+    end
+  end
 end
+ end
