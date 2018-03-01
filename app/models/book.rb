7d266ccee2eb644249b8e65b1694e6341778c774
Gaston <grodriguez@itba.edu.ar>
2018-03-01 14:24:59 -0300

    Fixed the book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
     has_many :rents
-    
+    validates :genre, presence: true 
+    validates :author, presence: true 
+    validates :image, presence: true 
+    validates :title, presence: true 
+    validates :publisher, presence: true 
+    validates :year, presence: true 
 end
