498c80afe6434e827d61f3742c67ce6fc7d7bbb2
Gaston <grodriguez@itba.edu.ar>
2018-03-02 08:30:30 -0300

    Fixing the factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 require 'faker'
 FactoryBot.define do
   factory :user do
-      uid        { Faker::Crypto.md5 }
+      #uid        { Faker::Crypto.md5 }
       sequence(:email) { |n| "person#{n}@example.com" }
       password   { Faker::Internet.password(8) }
       last_name  { Faker::Name.unique.first_name } 
