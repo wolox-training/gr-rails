35b2666ee87438aefdfa0f355e2375e3d8606c5d
Gaston <grodriguez@itba.edu.ar>
2018-03-02 14:33:59 -0300

    Fixed the destroys

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  has_many :rents, dependent: destroy
+  has_many :rents, dependent: :destroy
   validates :genre, :author, :image, :title, :publisher, :year, presence: true
 end
