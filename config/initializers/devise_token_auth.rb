18543f0fc9c7c8c30aac56fc38fda2ea3366b675
Gaston <grodriguez@itba.edu.ar>
2018-03-02 14:04:20 -0300

    Made the changes requested

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,4 +1,6 @@
 DeviseTokenAuth.setup do |config|
+  config.change_headers_on_each_request = false
+  config.default_confirm_success_url = Rails.application.secrets.confirm_success_url
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
