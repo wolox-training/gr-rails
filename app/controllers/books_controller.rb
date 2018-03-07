bd66b3221cda65aafe08988ee2305681b803c7a7
Gaston <grodriguez@itba.edu.ar>
2018-03-07 09:47:27 -0300

    Added authentication to the response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,18 @@
 class BooksController < ApplicationController
   include Wor::Paginate
+  
 
   def index
-    @books = Book.all
-    render_paginated @books
+    if user_signed_in?
+      @books = Book.all
+      render_paginated @books
+    end
   end
 
   def show
-    @books = Book.find(params[:id])
-    render json: @books
+    if user_signed_in?
+      @books = Book.find(params[:id])
+      render json: @books
+    end
   end
 end
