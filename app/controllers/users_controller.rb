803fc9d1d67e30f729a8ec77f7096cc43c28b4b8
Gaston <grodriguez@itba.edu.ar>
2018-03-07 11:20:49 -0300

    Starting the endpoints

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  #include Wor::Paginate
+
+  def rents
+    #if user_signed_in? && current_user.id == params[:user_id]
+      @rents = User.first.rents
+      #render_paginated @rents
+      render json: @rents
+
+    #else
+    #  render json: {}, status: 401
+    #end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'books/:id', to: 'books#show'
 
   devise_for :users
+
+  get 'users/:id/rents' => 'users#rents' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
  
