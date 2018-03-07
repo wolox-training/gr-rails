1e9085facb15e6abe9855229aa901a47564c8ae5
Gaston <grodriguez@itba.edu.ar>
2018-03-07 14:00:41 -0300

    Ran rubocop

--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :rent do
     user
     book
-    from    { Faker::Date.backward(10) } 
+    from    { Faker::Date.backward(10) }
     to      { Faker::Date.forward(10)  }
   end
 end
