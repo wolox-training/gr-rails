25d9eb81dcc2b9c384191b31edd6d7754ba9a8b3
Gaston <grodriguez@itba.edu.ar>
2018-03-09 09:29:04 -0300

    Re-added authentication at /api/v1/auth{...}

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ 
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
   root to: "home#index"
 end
