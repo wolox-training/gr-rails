05e4c9e659f68e2424f04467f9d153135c24ea49
Gaston <grodriguez@itba.edu.ar>
2018-03-14 09:05:17 -0300

    Fixed the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/:id', to: 'books#show'
+
   devise_for :users
-  resources :books, only: [:index, :show]
-  get 'books/:id/rents', to: 'books#rents'
-
-  resources :users do
-    resources :rents, only: [:index, :create, :show, :destroy]
-  end
-
-  namespace :api do
-    scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
-    end
-  end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
