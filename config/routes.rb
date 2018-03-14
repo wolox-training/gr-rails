9006932e3c4d5fffd86c8691f2282bd7593737fb
Gaston <grodriguez@itba.edu.ar>
2018-03-14 08:45:41 -0300

    Added the auth path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   get 'books/:id', to: 'books#show'
 
   devise_for :users
+  
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
